d0ad7a736b8b451266cf8027ec1fefb287479906
afontani <anthony.fontanini@nrel.gov>
2019-02-19 12:36:06 -0700

    Add prefix location for conda environment in /shared-projects/buildstock so conda does not load from /home/afontani directory.

--- buildstockbatch/eagle.sh
+++ buildstockbatch/eagle.sh
@@ -5,7 +5,10 @@
 echo "Job ID: $SLURM_JOB_ID"
 echo "Hostname: $HOSTNAME"
 
+CONDA_DIR=/shared-projects/buildstock/
+NAME=buildstock
+
 module load conda singularity-container
-source activate buildstock
+source activate $CONDA_DIR/$NAME
 
 time python -u -m buildstockbatch.eagle "$PROJECTFILE"
--- create_eagle_env.sh
+++ create_eagle_env.sh
@@ -5,9 +5,14 @@
 #SBATCH --nodes=1
 #SBATCH --time=15:00
 
+CONDA_DIR=/shared-projects/buildstock/
+NAME=buildstock
+
 module load conda
-conda remove -y --name buildstock --all
-conda create -y --name buildstock python=3.6 pandas hdf5 pytables
-source activate buildstock
+if [[ ! -d $CONDA_DIR/$NAME ]]; then
+    conda remove -y --prefix $CONDA_DIR/$NAME --all
+    conda create -y --prefix $CONDA_DIR/$NAME python=3.6 pandas hdf5 pytables
+fi
+source activate $CONDA_DIR/$NAME
 pip install --upgrade pip
 pip install -e .
