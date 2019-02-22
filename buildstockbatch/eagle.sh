2828a95bdfa8f970df747a7c237cce8ac8c538b7
Noel Merket <noel.merket@nrel.gov>
2019-02-22 09:30:13 -0700

    Revert "Add prefix location for conda environment in /shared-projects/buildstock so conda does not load from /home/afontani directory."
    This reverts commit d0ad7a736b8b451266cf8027ec1fefb287479906.

--- buildstockbatch/eagle.sh
+++ buildstockbatch/eagle.sh
@@ -5,10 +5,7 @@
 echo "Job ID: $SLURM_JOB_ID"
 echo "Hostname: $HOSTNAME"
 
-CONDA_DIR=/shared-projects/buildstock/
-NAME=buildstock
-
 module load conda singularity-container
-source activate $CONDA_DIR/$NAME
+source activate buildstock
 
 time python -u -m buildstockbatch.eagle "$PROJECTFILE"
--- create_eagle_env.sh
+++ create_eagle_env.sh
@@ -5,14 +5,9 @@
 #SBATCH --nodes=1
 #SBATCH --time=15:00
 
-CONDA_DIR=/shared-projects/buildstock/
-NAME=buildstock
-
 module load conda
-if [[ ! -d $CONDA_DIR/$NAME ]]; then
-    conda remove -y --prefix $CONDA_DIR/$NAME --all
-    conda create -y --prefix $CONDA_DIR/$NAME python=3.6 pandas hdf5 pytables
-fi
-source activate $CONDA_DIR/$NAME
+conda remove -y --name buildstock --all
+conda create -y --name buildstock python=3.6 pandas hdf5 pytables
+source activate buildstock
 pip install --upgrade pip
 pip install -e .
