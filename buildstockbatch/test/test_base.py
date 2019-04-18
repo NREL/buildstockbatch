a59aee28bc31ec6bf20c2c3a1e28e8253c4ced3c
Noel Merket <noel.merket@nrel.gov>
2019-04-18 15:57:28 -0600

    fixing postprocessing unit test

--- buildstockbatch/test/test_base.py
+++ buildstockbatch/test/test_base.py
@@ -22,7 +22,10 @@ def basic_residential_project_file():
         project_directory = 'project_resstock_national'
         os.makedirs(os.path.join(buildstock_directory, project_directory))
         output_directory = os.path.join(test_directory, 'output')
-        shutil.copytree(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_results'), output_directory)
+        shutil.copytree(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_results'),
+            os.path.join(output_directory, 'simulation_output')
+        )
 
         def _basic_residential_project_file(update_args={}):
             cfg = {
@@ -49,14 +52,19 @@ def test_missing_simulation_output_report_applicable(basic_residential_project_f
     project_filename, results_dir = basic_residential_project_file()
 
     # Modify the results to remove the simulation output report from all of one upgrade.
-    for item in os.listdir(results_dir):
-        datapoint_out_filename = os.path.join(results_dir, item, 'run', 'data_point_out.json')
-        if item.endswith('up01') and os.path.isfile(datapoint_out_filename):
-            with open(datapoint_out_filename, 'r') as f:
-                dpout = json.load(f)
-            del dpout['SimulationOutputReport']
-            with open(datapoint_out_filename, 'w') as f:
-                json.dump(dpout, f)
+    simout_dir = os.path.join(results_dir, 'simulation_output')
+    for upgrade_dir in os.listdir(simout_dir):
+        full_upgrade_dir = os.path.join(simout_dir, upgrade_dir)
+        if not os.path.isdir(full_upgrade_dir):
+            continue
+        for bldg_dir in os.listdir(full_upgrade_dir):
+            datapoint_out_filename = os.path.join(simout_dir, upgrade_dir, bldg_dir, 'run', 'data_point_out.json')
+            if upgrade_dir.endswith('up01') and os.path.isfile(datapoint_out_filename):
+                with open(datapoint_out_filename, 'r') as f:
+                    dpout = json.load(f)
+                del dpout['SimulationOutputReport']
+                with open(datapoint_out_filename, 'w') as f:
+                    json.dump(dpout, f)
 
     with patch.object(BuildStockBatchBase, 'weather_dir', None), \
             patch.object(BuildStockBatchBase, 'get_dask_client') as get_dask_client_mock, \
@@ -65,7 +73,7 @@ def test_missing_simulation_output_report_applicable(basic_residential_project_f
         bsb.process_results()
         get_dask_client_mock.assert_called_once()
 
-    up01_parquet = os.path.join(results_dir, 'results_up01.parquet')
+    up01_parquet = os.path.join(results_dir, 'parquet', 'upgrades', 'upgrade=1', 'results_up01.parquet')
     assert(os.path.exists(up01_parquet))
     df = pd.read_parquet(up01_parquet, engine='pyarrow')
     assert((~df['simulation_output_report.applicable']).any())
