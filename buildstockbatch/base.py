b9a0df913f4daebd8312f8aa8d6d92f9df3a1e95
Rajendra Adhikari <Rajendra.Adhikari@nrel.gov>
2019-04-22 16:00:45 -0600

    Added raw string indicator for regex search string

--- buildstockbatch/base.py
+++ buildstockbatch/base.py
@@ -468,7 +468,7 @@ class BuildStockBatchBase(object):
                     continue
                 new_pq = pd.read_parquet(full_path, engine='pyarrow')
 
-                building_id_match = re.search('bldg(\d+)', folder)
+                building_id_match = re.search(r'bldg(\d+)', folder)
                 assert building_id_match, f"The building results folder format should be: ~bldg(\\d+). Got: {folder} "
                 new_pq['building_id'] = int(building_id_match.group(1))
                 new_pq.rename(columns=lambda x: x.replace(':', '_').replace('[', "").replace("]", ""), inplace=True)
