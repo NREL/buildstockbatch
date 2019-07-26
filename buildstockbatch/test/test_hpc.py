8b50da356fa21466ed484bfcfd9d73d89c2a7cc2
Noel Merket <noel.merket@nrel.gov>
2019-07-26 09:49:20 -0600

    adding testing for docker/singularity images

--- buildstockbatch/test/test_hpc.py
+++ buildstockbatch/test/test_hpc.py
@@ -7,3 +7,9 @@ def test_singularity_image_download_url():
     url = HPCBatchBase.singularity_image_url()
     r = requests.head(url)
     assert r.status_code == requests.codes.ok
+
+    # To check for a docker image...
+    # Use a bearer tokean as at
+    # https://docs.docker.com/registry/spec/auth/token/
+    # https://docs.docker.com/registry/spec/api/
+
