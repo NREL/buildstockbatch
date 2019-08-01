ddd3850334d7afb1ff26274d109572c7a5d50287
Noel Merket <noel.merket@nrel.gov>
2019-08-01 16:50:48 -0600

    updating conda create line to install more packages with conda
    [skip ci]

--- create_eagle_env.sh
+++ create_eagle_env.sh
@@ -26,7 +26,7 @@ MY_CONDA_PREFIX="$CONDA_ENVS_DIR/$MY_CONDA_ENV_NAME"
 echo "Creating $MY_CONDA_PREFIX"
 module load conda
 conda remove -y --prefix "$MY_CONDA_PREFIX" --all
-conda create -y --prefix "$MY_CONDA_PREFIX" python=3.7 pandas hdf5
+conda create -y --prefix "$MY_CONDA_PREFIX" -c conda-forge "pyarrow>=0.14" python=3.7 pandas dask distributed
 source activate "$MY_CONDA_PREFIX"
 pip install --upgrade pip
 if [ $DEV -eq 1 ]
