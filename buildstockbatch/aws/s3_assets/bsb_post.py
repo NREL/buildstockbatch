8f3aea15d7e163338fdbbe7a8c4f0f3cb790deb7
David Rager <david.rager@nrel.gov>
2019-10-23 08:11:21 -0600

    Moving to new bucket threw off the EMR scripts - modifications added.

--- buildstockbatch/aws/s3_assets/bsb_post.py
+++ buildstockbatch/aws/s3_assets/bsb_post.py
@@ -1,31 +1,36 @@
 
-from dask_yarn import YarnCluster
-from dask.distributed import Client
+#from dask_yarn import YarnCluster
+#from dask.distributed import Client
 
-cluster = YarnCluster(
-    deploy_mode='local',
-    worker_vcores=2,
-    worker_memory='15 GB',
-    n_workers=16
-)
+#cluster = YarnCluster(
+#    deploy_mode='local',
+#    worker_vcores=2,
+#    worker_memory='15 GB',
+#    n_workers=16
+#)
 
-client = Client(cluster)
-from buildstockbatch.postprocessing import combine_results, create_athena_tables
+#client = Client(cluster)
+#from buildstockbatch.postprocessing import combine_results, create_athena_tables
 results_s3_loc = 's3://buildstockbatch-test5/ragertest1/results/'
 full_path = 'simulation_output/up01/bldg0000563/run/enduse_timeseries.parquet'
 
 from fs import open_fs
 import pandas as pd
-import os
-from fs.copy import copy_file
+#import os
+#from fs.copy import copy_file
 
 s3fs = open_fs(results_s3_loc)
+
+print(full_path)
+
+print(s3fs.listdir('.'))
 
 s3fs.getinfo(full_path).is_file
 
 with s3fs.open(full_path, 'rb') as f:
+    print(f)
     df = pd.read_parquet(f, engine='pyarrow')
-
+'''
 combine_results(results_s3_loc)
 
 conf = dict(
@@ -37,3 +42,4 @@ conf = dict(
 
 create_athena_tables(conf, buildstockbatch-test5, 'ragertest1/results/')
 
+'''
