8f76f208b5139bf1b8c92673ffc81e46ece39f8f
Andrew Parker <Andrew.Parker@nrel.gov>
2020-01-08 12:08:11 -0700

    Modifies tests to use LocalDocker and HPCBatch instances to test instance methods

--- buildstockbatch/test/test_hpc.py
+++ buildstockbatch/test/test_hpc.py
@@ -6,12 +6,15 @@ from unittest.mock import patch
 
 from buildstockbatch.hpc import HPCBatchBase
 
-
-def test_singularity_image_download_url():
-    hpc_batch_base = HPCBatchBase('TestHPCBatchBaseProject')
-    url = hpc_batch_base.singularity_image_url()
-    r = requests.head(url)
-    assert r.status_code == requests.codes.ok
+def test_singularity_image_download_url(basic_residential_project_file):
+    project_filename, results_dir = basic_residential_project_file()
+    with patch.object(HPCBatchBase, 'weather_dir', None), \
+            patch.object(HPCBatchBase, 'output_dir', results_dir), \
+            patch.object(HPCBatchBase, 'singularity_image', '/path/to/singularity.simg') as singularity_image:
+        hpc_batch_base = HPCBatchBase(project_filename)
+        url = hpc_batch_base.singularity_image_url
+        r = requests.head(url)
+        assert r.status_code == requests.codes.ok
 
 
 @patch('buildstockbatch.hpc.subprocess')
