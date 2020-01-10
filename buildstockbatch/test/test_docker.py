8c7c7f2d1a89c1479b213777cd9ac8c910f13ced
Andrew Parker <Andrew.Parker@nrel.gov>
2020-01-10 06:57:12 -0700

    Modifies test again to avoid unrelated docker hub connection error

--- buildstockbatch/test/test_docker.py
+++ buildstockbatch/test/test_docker.py
@@ -1,15 +1,19 @@
 import requests
 import re
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
-from buildstockbatch.localdocker import LocalDockerBatch
+from buildstockbatch.base import BuildStockBatchBase
 
 
 def test_docker_image_exists_on_docker_hub(basic_residential_project_file):
     project_filename, results_dir = basic_residential_project_file()
-    with patch.object(LocalDockerBatch, 'weather_dir', None):
-        local_docker_batch = LocalDockerBatch(project_filename)
-        docker_image, docker_tag = local_docker_batch.docker_image.split(':')
+    # Use a BuildStockBatchBase instance to get the version of OpenStudio
+    # because instantiating a LocalDockerBatch fails to connect
+    # with the docker website in the testing context for some reason.
+    with patch.object(BuildStockBatchBase, 'weather_dir', None):
+        bsb = BuildStockBatchBase(project_filename)
+        docker_image = 'nrel/openstudio'
+        docker_tag = bsb.os_version
         baseurl = 'https://registry.hub.docker.com/v2/'
         r1 = requests.get(baseurl)
         assert(r1.status_code == 401)
