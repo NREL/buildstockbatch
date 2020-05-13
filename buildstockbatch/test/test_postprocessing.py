a35866748db40a15cb3d7bfb70a42d3a2d3f48aa
aspeake1 <andrew.speake@nrel.gov>
2020-05-13 08:33:43 -0600

    test for new exception

--- buildstockbatch/test/test_postprocessing.py
+++ buildstockbatch/test/test_postprocessing.py
@@ -5,6 +5,8 @@ import pandas as pd
 import pathlib
 import re
 import tarfile
+import pytest
+import shutil
 
 from buildstockbatch import postprocessing
 from buildstockbatch.base import BuildStockBatchBase
@@ -55,3 +57,17 @@ def test_report_additional_results_csv_columns(basic_residential_project_file):
         assert (df['reporting_measure1.column_2'] == 2).all()
         assert (df['reporting_measure2.column_3'] == 3).all()
         assert (df['reporting_measure2.column_4'] == 4).all()
+
+def test_empty_results_assertion(basic_residential_project_file, capsys):
+    project_filename, results_dir = basic_residential_project_file({})
+
+    fs = LocalFileSystem()
+    results_dir = pathlib.Path(results_dir)
+    sim_out_dir = results_dir / 'simulation_output'
+    shutil.rmtree(sim_out_dir) #no results
+    cfg = BuildStockBatchBase.get_project_configuration(project_filename)    
+
+    with pytest.raises(ValueError) as except_info:
+        assert postprocessing.combine_results(fs, results_dir, cfg, do_timeseries=False)
+
+    assert str(except_info.value) == 'No simulation results found to post-process'
