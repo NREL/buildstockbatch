13563cd9de2e2a77a32f8c449f29b6e61ad6406c
Andrew Parker <Andrew.Parker@nrel.gov>
2020-12-17 15:26:16 -0700

    Handles ComStock and ResStock enduse_timeseries.csv columns, fixes #200

--- buildstockbatch/base.py
+++ buildstockbatch/base.py
@@ -247,7 +247,17 @@ class BuildStockBatchBase(object):
         timeseries_filepath = os.path.join(sim_dir, 'run', 'enduse_timeseries.csv')
         schedules_filepath = os.path.join(sim_dir, 'generated_files', 'schedules.csv')
         if os.path.isfile(timeseries_filepath):
-            tsdf = pd.read_csv(timeseries_filepath, parse_dates=['Time', 'TimeDST', 'TimeUTC'])
+            # Find the time columns present in the enduse_timeseries file
+            possible_time_cols = ['time', 'Time', 'TimeDST', 'TimeUTC']
+            cols = pd.read_csv(timeseries_filepath, index_col=False, nrows=0).columns.tolist()
+            actual_time_cols = []
+            for possible_time_col in possible_time_cols:
+                if possible_time_col in cols:
+                    actual_time_cols.append(possible_time_col)
+            if not actual_time_cols:
+                logger.error(f'Did not find any time column ({possible_time_cols}) in enduse_timeseries.csv.')
+                raise RuntimeError(f'Did not find any time column ({possible_time_cols}) in enduse_timeseries.csv.')
+            tsdf = pd.read_csv(timeseries_filepath, parse_dates=actual_time_cols)
             if os.path.isfile(schedules_filepath):
                 schedules = pd.read_csv(schedules_filepath)
                 schedules.rename(columns=lambda x: f'schedules_{x}', inplace=True)
@@ -399,8 +409,8 @@ class BuildStockBatchBase(object):
                         'TimeseriesCSVExport': 'timeseries_csv_export'
                         }
         if 'reporting_measures' in cfg.keys():
-            for reporting_measure in cfg['reporting_measures']:
-                measure_names[reporting_measure] = 'reporting_measures'
+            for reporting_measure in cfg.get('reporting_measures', []):
+                measure_names[reporting_measure['measure_dir_name']] = 'reporting_measures'
 
         error_msgs = ''
         warning_msgs = ''
