86960897ca4d7a408458b1fa9e3ccfc78fe4d047
Joe Robertson <joseph.robertson@nrel.gov>
2021-05-11 08:41:33 -0700

    Update simg to os320.

--- buildstockbatch/base.py
+++ buildstockbatch/base.py
@@ -39,8 +39,8 @@ logger = logging.getLogger(__name__)
 class BuildStockBatchBase(object):
 
     # http://openstudio-builds.s3-website-us-east-1.amazonaws.com
-    DEFAULT_OS_VERSION = '3.1.0'
-    DEFAULT_OS_SHA = 'e165090621'
+    DEFAULT_OS_VERSION = '3.2.0'
+    DEFAULT_OS_SHA = 'e11f0a08b2'
     CONTAINER_RUNTIME = None
     LOGO = '''
      _ __         _     __,              _ __
