45cf6c1106ef5550bdf579b84a1c942c67c32b86
Anthony Fontanini <Anthony.Fontanini@nrel.gov>
2021-05-21 14:54:10 -0600

    Switch source activate to conda deactivate

--- create_eagle_env.sh
+++ create_eagle_env.sh
@@ -27,7 +27,7 @@ echo "Creating $MY_CONDA_PREFIX"
 module load conda
 conda remove -y --prefix "$MY_CONDA_PREFIX" --all
 conda create -y --prefix "$MY_CONDA_PREFIX" -c conda-forge "pyarrow>=3.0.0" python=3.7.8 "numpy>=1.20.0" "pandas>=1.0.0,!=1.0.4" dask distributed ruby "click<8.0.0"  # Remove click after this is fixed https://github.com/dask/distributed/issues/4817
-source deactivate 
+conda deactivate 
 source activate "$MY_CONDA_PREFIX"
 which pip
 if [ $DEV -eq 1 ]
