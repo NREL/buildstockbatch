8b7233e1d3e43a3e95843bbc410957be9b3894a5
Joe Robertson <joseph.robertson@nrel.gov>
2022-01-27 16:51:43 -0700

    Relax requires on fuel args.

--- buildstockbatch/workflow_generator/residential_hpxml.py
+++ buildstockbatch/workflow_generator/residential_hpxml.py
@@ -42,10 +42,6 @@ class ResidentialHpxmlWorkflowGenerator(WorkflowGeneratorBase):
             scenario_name: str(required=True)
             type: str(required=True)
             elec_folder: str(required=True)
-            gas_value: str(required=False)
-            propane_value: str(required=False)
-            oil_value: str(required=False)
-            wood_value: str(required=False)
         measure-spec:
             measure_dir_name: str(required=True)
             arguments: map(required=False)
