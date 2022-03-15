a97f2bb5d2147fb3c20ff11b9c5d31ed716e5819
Joe Robertson <joseph.robertson@nrel.gov>
2022-03-15 15:59:35 -0700

    Stub changes for new output_variables argument.

--- buildstockbatch/workflow_generator/residential_hpxml.py
+++ buildstockbatch/workflow_generator/residential_hpxml.py
@@ -121,6 +121,10 @@ class ResidentialHpxmlWorkflowGenerator(WorkflowGeneratorBase):
         }
         sim_out_rep_args.update(workflow_args['simulation_output_report'])
 
+        if 'output_variables' in sim_out_rep_args:
+            output_variables = sim_out_rep_args['output_variables']
+            sim_out_rep_args['output_variables'] = ','.join([str(s.get('name')) for s in output_variables])
+
         osw = {
             'id': sim_id,
             'steps': [
