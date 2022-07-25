a7786e08819f8598d74251e1d0a4d126a5603104
Joe Robertson <joseph.robertson@nrel.gov>
2022-07-25 11:36:48 -0700

    Update test.

--- buildstockbatch/workflow_generator/test_workflow_generator.py
+++ buildstockbatch/workflow_generator/test_workflow_generator.py
@@ -359,7 +359,7 @@ def test_residential_hpxml(mocker):
     osw = osw_gen.create_osw(sim_id, building_id, upgrade_idx)
 
     steps = osw['steps']
-    assert(len(steps) == 6)
+    assert(len(steps) == 7)
 
     build_existing_model_step = steps[0]
     assert(build_existing_model_step['measure_dir_name'] == 'BuildExistingModel')
