50125e37b774e55145e96e55314a6b8ce5e6b3fa
aspeake1 <andrew.speake@nrel.gov>
2022-11-10 18:37:41 -0700

    update hes workflow generator test

--- buildstockbatch/workflow_generator/test_workflow_generator.py
+++ buildstockbatch/workflow_generator/test_workflow_generator.py
@@ -451,7 +451,7 @@ def test_residential_hpxml_hes(mocker):
     osw = osw_gen.create_osw(sim_id, building_id, upgrade_idx)
 
     steps = osw['steps']
-    assert len(steps) == 6
+    assert len(steps) == 8
 
     build_existing_model_step = steps[0]
     assert build_existing_model_step['measure_dir_name'] == 'BuildExistingModel'
@@ -460,25 +460,35 @@ def test_residential_hpxml_hes(mocker):
     assert build_existing_model_step['arguments']['simulation_control_run_period_end_month'] == 2
     assert build_existing_model_step['arguments']['simulation_control_run_period_end_day_of_month'] == 28
     assert build_existing_model_step['arguments']['simulation_control_run_period_calendar_year'] == 2010
-    assert build_existing_model_step['arguments']['os_hescore_directory'] == "../../OpenStudio-HEScore"
+    assert build_existing_model_step['arguments']['os_hescore_directory'] == "/opt/OpenStudio-HEScore"
 
     apply_upgrade_step = steps[1]
     assert apply_upgrade_step['measure_dir_name'] == 'ApplyUpgrade'
 
-    simulation_output_step = steps[2]
+    apply_upgrade_step = steps[2]
+    assert apply_upgrade_step['measure_dir_name'] == 'HPXMLtoOpenStudio'
+    assert apply_upgrade_step['arguments']['hpxml_path'] == '/var/simdata/openstudio/run/home.xml'
+    assert apply_upgrade_step['arguments']['output_dir'] == '/var/simdata/openstudio/run'
+    assert apply_upgrade_step['arguments']['debug'] is False
+    assert apply_upgrade_step['arguments']['add_component_loads'] is False
+
+    simulation_output_step = steps[3]
     assert simulation_output_step['measure_dir_name'] == 'ReportSimulationOutput'
     assert simulation_output_step['arguments']['timeseries_frequency'] == 'hourly'
     assert simulation_output_step['arguments']['include_timeseries_end_use_consumptions'] is True
     assert simulation_output_step['arguments']['include_timeseries_total_loads'] is True
     assert simulation_output_step['arguments']['include_timeseries_zone_temperatures'] is False
 
-    hpxml_output_step = steps[3]
+    hpxml_output_step = steps[4]
     assert hpxml_output_step['measure_dir_name'] == 'ReportHPXMLOutput'
 
-    upgrade_costs_step = steps[4]
+    hpxml_output_step = steps[5]
+    assert hpxml_output_step['measure_dir_name'] == 'ReportUtilityBills'
+
+    upgrade_costs_step = steps[6]
     assert upgrade_costs_step['measure_dir_name'] == 'UpgradeCosts'
 
-    server_dir_cleanup_step = steps[5]
+    server_dir_cleanup_step = steps[7]
     assert server_dir_cleanup_step['measure_dir_name'] == 'ServerDirectoryCleanup'
 
 
