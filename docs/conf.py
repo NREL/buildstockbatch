410ac347dbbe0d6d889a9a0026cce1658e30cfdb
Joe Robertson <joseph.robertson@nrel.gov>
2022-12-07 09:19:11 -0700

    Update conf.

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ language = 'en'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -116,8 +116,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = ["_themes", ]
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -128,7 +129,13 @@ html_theme_path = ["_themes", ]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['nstatic/']
+
+def setup(app):
+    try:
+        app.add_css_file("stylesheet.css")
+    except:
+        app.add_stylesheet("stylesheet.css")
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
