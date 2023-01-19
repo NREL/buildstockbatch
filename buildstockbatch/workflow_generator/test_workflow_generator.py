ae74eabfe71cfd9b8fb1fc60ae0d7f6bb9406beb
Joe Robertson <joseph.robertson@nrel.gov>
2023-01-19 12:41:42 -0700

    Comment out new args in wfg test.

--- buildstockbatch/workflow_generator/test_workflow_generator.py
+++ buildstockbatch/workflow_generator/test_workflow_generator.py
@@ -388,12 +388,12 @@ def test_residential_hpxml(mocker):
     assert simulation_output_step['arguments']['include_timeseries_hot_water_uses'] is False
     assert simulation_output_step['arguments']['include_timeseries_total_loads'] is True
     assert simulation_output_step['arguments']['include_timeseries_component_loads'] is False
-    assert simulation_output_step['arguments']['include_timeseries_unmet_hours'] is False
+    # assert simulation_output_step['arguments']['include_timeseries_unmet_hours'] is False
     assert simulation_output_step['arguments']['include_timeseries_zone_temperatures'] is False
     assert simulation_output_step['arguments']['include_timeseries_airflows'] is False
     assert simulation_output_step['arguments']['include_timeseries_weather'] is False
     assert simulation_output_step['arguments']['timeseries_timestamp_convention'] == 'end'
-    assert simulation_output_step['arguments']['timeseries_num_decimal_places'] == 5
+    # assert simulation_output_step['arguments']['timeseries_num_decimal_places'] == 5
     assert simulation_output_step['arguments']['add_timeseries_dst_column'] is True
     assert simulation_output_step['arguments']['add_timeseries_utc_column'] is True
 
