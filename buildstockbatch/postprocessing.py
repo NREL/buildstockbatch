b9f23dd7563126cfc9e68d454d9457a93d869a4d
Noel Merket <noel.merket@nrel.gov>
2024-03-21 11:11:31 -0600

    adding a checkpoint to skip upgrades already complete.

--- buildstockbatch/postprocessing.py
+++ buildstockbatch/postprocessing.py
@@ -405,7 +405,7 @@ def combine_results(fs, results_dir, cfg, do_timeseries=True):
 
     # create the postprocessing results directories
     for dr in dirs:
-        fs.makedirs(dr)
+        fs.makedirs(dr, exist_ok=True)
 
     # Results "CSV"
     results_json_files = fs.glob(f"{sim_output_dir}/results_job*.json.gz")
@@ -457,6 +457,15 @@ def combine_results(fs, results_dir, cfg, do_timeseries=True):
 
     results_df_groups = results_df.groupby("upgrade")
     upgrade_list = get_upgrade_list(cfg)
+    checkpoint_filename = f"{results_dir}/checkpoints.json"
+    if fs.exists(checkpoint_filename):
+        with fs.open(checkpoint_filename, "r") as f:
+            checkpoint = json.load(f)
+    else:
+        checkpoint = {"upgrades_processed": []}
+    for upgrade_id in checkpoint["upgrades_processed"]:
+        logger.info(f"Upgrade {upgrade_id} has already been processed, skipping.")
+    upgrade_list = sorted(set(upgrade_list).difference(checkpoint["upgrades_processed"]))
     partition_columns = cfg.get("postprocessing", {}).get("partition_columns", [])
     partition_columns = [c.lower() for c in partition_columns]
     df_partition_columns = [f"build_existing_model.{c}" for c in partition_columns]
@@ -596,7 +605,10 @@ def combine_results(fs, results_dir, cfg, do_timeseries=True):
                     )
 
             logger.info(f"Finished combining and saving timeseries for upgrade{upgrade_id}.")
-    logger.info("All aggregation completed. ")
+        checkpoint["upgrades_processed"].append(upgrade_id)
+        with fs.open(checkpoint_filename, "w") as f:
+            json.dump(checkpoint, f)
+    logger.info("All aggregation completed.")
     if do_timeseries:
         logger.info("Writing timeseries metadata files")
         write_metadata_files(fs, ts_dir, partition_columns)
