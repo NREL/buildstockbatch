aeb9750bcb9ed43ae7000b201fe54a7bdfa71771
Philip White <pwhite@kl3.head.cm.kestrel.hpc.nrel.gov>
2024-07-05 11:48:45 -0600

    removal df related lines postprocessing

--- buildstockbatch/kestrel_postprocessing.sh
+++ buildstockbatch/kestrel_postprocessing.sh
@@ -6,8 +6,6 @@ echo "begin kestrel_postprocessing.sh"
 echo "Job ID: $SLURM_JOB_ID"
 echo "Hostname: $HOSTNAME"
 
-df -i
-df -h
 
 module load python apptainer
 source "$MY_PYTHON_ENV/bin/activate"
@@ -30,8 +28,6 @@ echo $SLURM_JOB_NODELIST_PACK_GROUP_0
 echo "workers"
 echo $SLURM_JOB_NODELIST_PACK_GROUP_1
 
-pdsh -w $SLURM_JOB_NODELIST_PACK_GROUP_1 "free -h"
-pdsh -w $SLURM_JOB_NODELIST_PACK_GROUP_1 "df -i; df -h"
 
 $MY_PYTHON_ENV/bin/dask scheduler --scheduler-file $SCHEDULER_FILE &> $OUT_DIR/dask_scheduler.out &
 pdsh -w $SLURM_JOB_NODELIST_PACK_GROUP_1 "source /kfs2/shared-projects/buildstock/aws_credentials.sh; $MY_PYTHON_ENV/bin/dask worker --scheduler-file $SCHEDULER_FILE --local-directory $LOCAL_SCRATCH/dask --nworkers ${NPROCS} --nthreads 1 --memory-limit ${MEMORY}MB" &> $OUT_DIR/dask_workers.out &
