d6a09f99889bfb9572cf93a7c8b93788b7d17df6
Joe Robertson <joseph.robertson@nrel.gov>
2024-07-17 09:30:19 -0700

    Try to fix the arg mapping.

--- buildstockbatch/workflow_generator/residential/residential_hpxml_arg_mapping.py
+++ buildstockbatch/workflow_generator/residential/residential_hpxml_arg_mapping.py
@@ -63,7 +63,9 @@ ARG_MAP = {
         "simulation_output_report": {
             "output_variables": "user_output_variables",
         },
-        "ReportUtilityBills": {
+    },
+    "ReportUtilityBills": {
+        "simulation_output_report": {
             "include_annual_bills": "register_annual_bills",
             "include_monthly_bills": "register_monthly_bills",
         },
