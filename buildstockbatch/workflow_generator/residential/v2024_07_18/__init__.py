3489e6bc69a7dc625e12100c19f05967ad39f6cc
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2024-07-18 15:17:19 -0500

    Add older version

---
+++ buildstockbatch/workflow_generator/residential/v2024_07_18/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "2024.07.18"
+version_info = {
+    "version": __version__,
+    "minimum_resstock_version": "3.2.0",  # Inclusive - works with 3.2.0
+    "maximum_resstock_version": "3.3.0",  # Exclusive - doesn't work with 3.3.0
+    "version_description": """
+This is the first version since the implementation of WorkflowGenerator versioning.
+This version is compatible with ResStock v3.2.0 until v3.3.0.
+""",
+}
