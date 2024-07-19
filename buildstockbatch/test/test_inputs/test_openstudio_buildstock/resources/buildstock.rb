83deb33a37b1889da8b528decd2cd30f9d1f5bf8
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2024-07-19 10:21:38 -0500

    Add Workflowgenerator version to test buildstock.rb

--- buildstockbatch/test/test_inputs/test_openstudio_buildstock/resources/buildstock.rb
+++ buildstockbatch/test/test_inputs/test_openstudio_buildstock/resources/buildstock.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 class Version
-  ResStock_Version = '2000.1.0' # Version of ResStock
+  ResStock_Version = '3.1.0' # Version of ResStock
   BuildStockBatch_Version = '2023.5.0' # Minimum required version of BuildStockBatch
+  WorkflowGenerator_Version = '2024.07.19'
   def self.check_buildstockbatch_version
     if ENV.keys.include?('BUILDSTOCKBATCH_VERSION') # buildstockbatch is installed
       bsb_version = ENV['BUILDSTOCKBATCH_VERSION']
