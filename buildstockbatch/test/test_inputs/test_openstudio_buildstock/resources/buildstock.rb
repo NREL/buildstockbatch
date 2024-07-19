bef335d301230d6c59c8c46df086f3082f87c16c
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2024-07-19 10:58:46 -0500

    Use WG version .18

--- buildstockbatch/test/test_inputs/test_openstudio_buildstock/resources/buildstock.rb
+++ buildstockbatch/test/test_inputs/test_openstudio_buildstock/resources/buildstock.rb
@@ -2,7 +2,7 @@
 class Version
   ResStock_Version = '3.1.0' # Version of ResStock
   BuildStockBatch_Version = '2023.5.0' # Minimum required version of BuildStockBatch
-  WorkflowGenerator_Version = '2024.07.19'
+  WorkflowGenerator_Version = '2024.07.18'
   def self.check_buildstockbatch_version
     if ENV.keys.include?('BUILDSTOCKBATCH_VERSION') # buildstockbatch is installed
       bsb_version = ENV['BUILDSTOCKBATCH_VERSION']
