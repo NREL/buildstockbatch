7d16c4164dd00f3fd9df53194ba4f01f4ab33874
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2024-07-19 10:23:46 -0500

    Edit version description text

--- buildstockbatch/workflow_generator/residential/latest/__init__.py
+++ buildstockbatch/workflow_generator/residential/latest/__init__.py
@@ -2,9 +2,7 @@ __version__ = "2024.07.19"
 version_info = {
     "version": __version__,
     "version_description": """
-Version information for the residential workflow generator.
-This version is compatible with ResStock v3.2.0 and later.
 In this version, the UpgradeCosts measure is converted to a regular measure
-and is run early in the workflow.
+and is run early in the workflow. ReportHPXMLOutput measure is no longer used.
 """,
 }
--- buildstockbatch/workflow_generator/residential/v2024_07_18/__init__.py
+++ buildstockbatch/workflow_generator/residential/v2024_07_18/__init__.py
@@ -3,6 +3,5 @@ version_info = {
     "version": __version__,
     "version_description": """
 This is the first version since the implementation of WorkflowGenerator versioning.
-This version is compatible with ResStock v3.2.0 until v3.3.0.
 """,
 }
