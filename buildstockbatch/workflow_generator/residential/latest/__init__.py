30b8ac28d9f236632e0abbbdb259e8cdf2c6d4e5
Rajendra Adhikari <rajendraadhikari.ee@gmail.com>
2024-07-18 20:24:11 -0500

    Get rid of min/max resstock version

--- buildstockbatch/workflow_generator/residential/latest/__init__.py
+++ buildstockbatch/workflow_generator/residential/latest/__init__.py
@@ -1,8 +1,6 @@
 __version__ = "2024.07.19"
 version_info = {
     "version": __version__,
-    "minimum_resstock_version": "3.2.0",
-    "maximum_resstock_version": None,
     "version_description": """
 Version information for the residential workflow generator.
 This version is compatible with ResStock v3.2.0 and later.
